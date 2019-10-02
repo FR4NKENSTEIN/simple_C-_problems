6de71eea5ecbbd436bedfcc8f3ad6ff66a093290
FR4NKENSTEIN <jacobleesmithy@gmail.com>
2019-10-02 15:02:59 -0700

    initial Commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Funds_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // print number 1 to 255
+            // for (int i = 1; i <= 255; i++)
+            // {
+            //     Console.WriteLine(i);
+            // }
+
+            //print numbers 1 to 100 only divisible by 3 or 5
+            // for (int i = 1; i <= 100; i++)
+            // {
+            //     if (i%3 == 0 && i%5 == 0)
+            //     {
+            //         System.Console.WriteLine("no no no");
+            //     }
+            //     else if (i%3 == 0)
+            //     {
+            //         System.Console.WriteLine(i);
+            //     }
+            //     else if (i%5 == 0)
+            //     {
+            //         System.Console.WriteLine(i);
+            //     }
+            // }
+
+            //print "Fizz" if i%3=0 print "Buzz" if i%5=0 print "FizzBuzz" if both are true
+            // for (int i = 1; i <= 100; i++)
+            // {
+            //     if (i%3 == 0 && i%5 == 0)
+            //     {
+            //         System.Console.WriteLine("FizzBuzz");
+            //     }
+            //     else if (i%3 == 0)
+            //     {
+            //         System.Console.WriteLine("Fizz");
+            //     }
+            //     else if (i%5 == 0)
+            //     {
+            //         System.Console.WriteLine("Buzz");
+            //     }
+            // }
+        }
+    }
+}
